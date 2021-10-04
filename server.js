841e45ab976d2d3f728be0d9281dffec43e9d685
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-04 16:06:22 -0400

    added db.query in server.js to return databse

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,11 +8,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username
+        user: 'root',
+        //your sql password
+        password: 'michelle12',
+        database: 'election'
+    },
+    console.log('connected to the election database')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 //handle user requests that aren't supported by the app 
 //default response for any other request (not found) 
 //make sure this is last get test route
+//catchall route
 app.use((req, res) => {
     res.status(404).end();
 });
