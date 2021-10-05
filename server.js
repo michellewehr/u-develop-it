011877cc0504673b7b6086546b4639b03d4958bb
Michelle Wehr <michelle_wehr@icloud.com>
2021-10-04 20:12:43 -0400

    learned to join tables

--- server.js
+++ server.js
@@ -24,7 +24,11 @@ const db = mysql.createConnection(
 
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id`;
   
     db.query(sql, (err, rows) => {
       if (err) {
@@ -40,7 +44,13 @@ app.get('/api/candidates', (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+    AS party_name 
+    FROM candidates 
+    LEFT JOIN parties 
+    ON candidates.party_id = parties.id 
+    WHERE candidates.id = ?`;
+    
     const params = [req.params.id];
   
     db.query(sql, params, (err, row) => {
@@ -100,16 +110,7 @@ app.post('/api/candidate', ({ body }, res) => {
     })
   });
 
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-// VALUES (?, ?, ?, ?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
 
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// })
 
 //handle user requests that aren't supported by the app 
 //default response for any other request (not found) 
